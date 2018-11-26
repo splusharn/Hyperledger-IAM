25f2a4ad70fb23f85ed9dbb28ceda9890e4b7a0f
splusharn <splusharn@gmail.com>
2018-11-26 20:36:19 +0100

    testing and data gathering change RAND

---
+++ fabric-scripts/composerSubmitLoop.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+COUNT=0
+
+while true
+do
+RAND=$(shuf -i 1-100000 -n 1)
+composer transaction submit --card admin@idmnetwork --data '{"$class":"org.acme.mynetwork.CreateGroup","groupName":"'"$RAND"'"}'
+#curl -X GET --header 'Accept: application/json' 'http://localhost:3000/api/system/ping'
+#composer transaction submit --card admin@idmnetwork -v
+COUNT=$((COUNT+1))
+echo "Transaction NR: $COUNT rand: $RAND"
+done
