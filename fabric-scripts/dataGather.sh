db170b5f8a5ffa6688794b546fbc7cddac87beba
splusharn <splusharn@gmail.com>
2018-12-02 22:03:28 +0100

    dataGather and soap changes

--- fabric-scripts/dataGather.sh
+++ fabric-scripts/dataGather.sh
@@ -9,6 +9,7 @@ sar -u -r -n DEV -d -b 5 >> $SYSFILE &
 while true
 do
 docker stats -a --no-stream >> $DOCKERFILE
-ps -Ao user,args,comm,pid,pcpu,pmem --sort=-pcpu | head -n 8 >> $PSFILE
+#ps -Ao user,args,comm,pid,pcpu,pmem --sort=-pcpu | head -n 8 >> $PSFILE
+top -b -c -H | head -n 20 | tail -n 14 >> $PSFILE
 sleep 5
 done
